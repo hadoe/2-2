09cce5672513f64a693d891c89cd25e76ddc7959
hadoe <66904673+hadoe@users.noreply.github.com>
2026-04-03 12:29:44 +0300

    Add files via upload

---
+++ 2-2/FilePrint.cpp
@@ -0,0 +1,20 @@
+﻿#include "FilePrint.h"
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+
+FilePrinter::FilePrinter(const std::string& fname) : filename(fname) {}
+
+void FilePrinter::printNumbers() const {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Ошибка: не удалось открыть файл " << filename << std::endl;
+        return;
+    }
+
+    std::istream_iterator<int> input_it(file);
+    std::istream_iterator<int> end_it;
+    std::ostream_iterator<int> output_it(std::cout, " ");
+    std::copy(input_it, end_it, output_it);
+}
---
+++ 2-2/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "FilePrint.h"
+#include <iostream>
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    std::string filename;
+    std::cout << "Введите имя файла: ";
+    std::getline(std::cin, filename);
+
+    FilePrinter printer(filename);
+    printer.printNumbers();
+
+    return 0;
+}
