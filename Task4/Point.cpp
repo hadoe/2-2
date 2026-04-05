468badd64e24a9c173074122942a7d3936cbeea6
Dany Smirnov <dany-s.smirnov2015@yandex.ru>
2026-04-05 15:41:09 +0300

    Задание 4

---
+++ Task4/Point.cpp
@@ -0,0 +1,38 @@
+﻿#include "Point.h"
+#include <sstream>
+
+point::point(int x, int y, const std::string& s) : x(x), y(y), s(s) {}
+
+bool point::operator<(const point& other) const
+{
+    return x < other.x || (x == other.x && y < other.y);
+}
+
+bool point::operator>(const point& other) const
+{
+    return other < *this;
+}
+
+std::istream& operator>>(std::istream& is, point& p)
+{
+    is >> p.x >> p.y >> p.s;
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const point& p)
+{
+    os << p.x << " " << p.y << " " << p.s;
+    return os;
+}
+
+point::operator std::string() const
+{
+    std::ostringstream oss;
+    oss << x << " " << y << " " << s;
+    return oss.str();
+}
+
+bool point::is_positive() const
+{
+    return x > 0 && y > 0;
+}
---
+++ Task4/Point.h
@@ -0,0 +1,89 @@
+﻿#ifndef POINT_H
+#define POINT_H
+
+#include <iostream>
+#include <string>
+
+/**
+ * @brief Структура, представляющая точку с координатами x, y и строкой s.
+ */
+struct point
+{
+    int x;          ///< Координата x
+    int y;          ///< Координата y
+    std::string s;  ///< Строковое представление
+
+    /**
+     * @brief Конструктор по умолчанию.
+     */
+    point() : x(0), y(0), s("") {}
+
+    /**
+     * @brief Конструктор с параметрами.
+     *
+     * @param x Координата x.
+     * @param y Координата y.
+     * @param s Строковое представление.
+     */
+    point(int x, int y, const std::string& s);
+
+    /**
+     * @brief Оператор сравнения: A < B.
+     *
+     * A < B, если A.x < B.x или (A.x == B.x и A.y < B.y).
+     *
+     * @param other Точка для сравнения.
+     * @return true, если текущая точка меньше other.
+     */
+    bool operator<(const point& other) const;
+
+    /**
+     * @brief Оператор сравнения: A > B.
+     *
+     * @param other Точка для сравнения.
+     * @return true, если текущая точка больше other.
+     */
+    bool operator>(const point& other) const;
+
+    /**
+     * @brief Оператор ввода из потока.
+     *
+     * Последовательно считывает x, y и s из входного потока is.
+     *
+     * @param is Входной поток.
+     * @param p Точка для заполнения.
+     * @return Ссылка на входной поток.
+     */
+    friend std::istream& operator>>(std::istream& is, point& p);
+
+    /**
+     * @brief Оператор вывода в поток.
+     *
+     * Записывает все члены структуры p в выходной поток os.
+     * Между членами вставляется пробел.
+     *
+     * @param os Выходной поток.
+     * @param p Точка для вывода.
+     * @return Ссылка на выходной поток.
+     */
+    friend std::ostream& operator<<(std::ostream& os, const point& p);
+
+    /**
+     * @brief Оператор преобразования в строку.
+     *
+     * Возвращает строку с текстовыми представлениями членов x, y, s,
+     * разделёнными пробелами.
+     *
+     * @return Строковое представление точки.
+     */
+    operator std::string() const;
+
+    /**
+     * @brief Проверка, являются ли x и y положительными.
+     *
+     * @return true, если x > 0 и y > 0.
+     */
+    bool is_positive() const;
+};
+
+#endif // POINT_H
